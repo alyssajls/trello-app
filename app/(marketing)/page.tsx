b36f5bb0276ce6bfab8261dac49937c6cdf23ab1
alyssajls <alyssajls@naver.com>
2024-02-23 17:37:35 +0900

    day1

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,9 @@
+const MartketingPage = () => {
+  return(
+    <div className="flex items-center justify-center flex-col">
+        Marketing Page;
+    </div>
+  ); 
+};
+
+export default MartketingPage;
